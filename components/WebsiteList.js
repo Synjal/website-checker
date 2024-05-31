f7e2ac2bfbac96214cfe1be166b3c803b61772bc
Synjal <frau.nicolas7@gmail.com>
2024-05-31 16:13:32 +0200

    add grid display

--- components/WebsiteList.js
+++ components/WebsiteList.js
@@ -1,27 +1,78 @@
 import {FlatList, View, Text, StyleSheet} from "react-native";
-import {Icon} from "react-native-paper";
-import {useContext} from "react";
+import {Icon, SegmentedButtons} from "react-native-paper";
+import {useContext, useState} from "react";
 import {ThemeContext} from "../context/ThemeContext";
 
 const WebsiteList = ({ data }) => {
     const { theme } = useContext(ThemeContext)
 
+    const [value, setValue] = useState('list')
+
+    const [isSwitchOn, setIsSwitchOn] = useState(false)
+    const onToggleSwitch = () => {
+        setIsSwitchOn(!isSwitchOn)
+
+    }
+
     return (
         <View style={styles.listContainer(theme)}>
-            <FlatList
-                data={data}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => (
-                    <View style={styles.itemContainer(theme)}>
-                        <Text style={styles.websiteName(theme)}>{item.name}</Text>
-                        <Icon
-                            source= {item.online === "on" ? "wifi" : "wifi-off" }
-                            color={ item.online === "on" ? theme.on : theme.off}
-                            size={40}
-                        />
-                    </View>
-                )}
+            <SegmentedButtons
+                value={value}
+                onValueChange={setValue}
+                buttons={[
+                    {
+                        icon: 'view-list-outline',
+                        label: 'Liste',
+                        value: 'list',
+                        checkedColor: theme.secondary,
+                        uncheckedColor: theme.onBackground
+                    },
+                    {
+                        icon: 'view-grid-outline',
+                        label: 'Grille',
+                        value: 'grid',
+                        checkedColor: theme.secondary,
+                        uncheckedColor: theme.onBackground
+                    },
+                ]}
+                style={{marginBottom: 20}}
             />
+
+            {value === 'list' &&
+                <FlatList
+                    data={data}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <View style={styles.itemContainer(theme)}>
+                            <Text style={styles.websiteName(theme)}>{item.name}</Text>
+                            <Icon
+                                source={item.online === "on" ? "wifi" : "wifi-off"}
+                                color={item.online === "on" ? theme.on : theme.off}
+                                size={40}
+                            />
+                        </View>
+                    )}
+                />
+            }
+
+            {value === 'grid' &&
+                <FlatList
+                    data={data}
+                    keyExtractor={(item, index) => index.toString()}
+                    numColumns={2}
+                    renderItem={({ item }) => (
+                        <View style={styles.itemContainerGrid(theme)}>
+                            <Text style={styles.websiteNameGrid(theme)}>{item.name}</Text>
+                            <Icon
+                                source={item.online === "on" ? "wifi" : "wifi-off"}
+                                color={item.online === "on" ? theme.on : theme.off}
+                                size={40}
+                            />
+                        </View>
+                    )}
+                    columnWrapperStyle={styles.row}
+                />
+            }
         </View>
     )
 }
@@ -29,6 +80,10 @@ const WebsiteList = ({ data }) => {
 export default WebsiteList;
 
 const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
     listContainer: theme => ({
         flex: 1,
         margin: 20,
@@ -41,7 +96,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent:'space-between',
         borderBottomWidth: 0.5,
-        borderBottomColor: theme.onBackground
+        borderBottomColor: theme.primary
     }),
     websiteName: theme => ({
         fontSize: 18,
@@ -49,5 +104,20 @@ const styles = StyleSheet.create({
         fontWeight: '400',
         textTransform: 'uppercase',
         color: theme.onBackground
+    }),
+    itemContainerGrid: theme => ({
+        flex: 1,
+        margin: 10,
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.surface, // Assuming you have an itemBackground property in your theme
+        borderRadius: 10,
+        height: 150, // Ensuring the item is a square
+    }),
+    websiteNameGrid: theme => ({
+        fontSize: 18,
+        color: theme.textColor,
+        marginBottom: 10,
     }),
 });
