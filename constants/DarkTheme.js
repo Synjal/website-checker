61e9ab279ff1d64280b0b99efffc20c833a5896f
Synjal <frau.nicolas7@gmail.com>
2024-05-31 15:32:10 +0200

    add themes

---
+++ constants/DarkTheme.js
@@ -0,0 +1,16 @@
+export const DarkTheme = {
+    primary: '#b78bbd',
+    onPrimary: '#000000',
+    secondary: '#654070',
+    onSecondary: '#000000',
+    tertiary: '#7d7a80',
+    onTertiary: '#000000',
+    background: "rgb(47,47,47)",
+    onBackground: "rgb(231, 225, 229)",
+    surface: "rgb(74, 69, 78)",
+    onSurface: "rgb(47,47,47)",
+    error: '#a83333',
+    onError: '#000000',
+    on: "#5e8f56",
+    off: "#b24d4d"
+}
---
+++ constants/LightTheme.js
@@ -0,0 +1,16 @@
+export const LightTheme = {
+    primary: '#7116B6',
+    onPrimary: '#fcfcfc',
+    secondary: '#A86EAF',
+    onSecondary: '#fcfcfc',
+    tertiary: '#363537',
+    onTertiary: '#fcfcfc',
+    background: '#FFF5EBFF',
+    onBackground: '#000000',
+    surface: "#FCE9D6",
+    onSurface: '#000000',
+    error: '#8f1d1d',
+    onError: '#fcfcfc',
+    on: "#2c8f1d",
+    off: "#8f1d1d"
+}
