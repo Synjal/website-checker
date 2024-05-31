04cbf1fd209b47953ec86c12c2dc07003291d610
Synjal <frau.nicolas7@gmail.com>
2024-05-31 15:32:44 +0200

    fix theme on top of navigation

--- App.js
+++ App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './app/Home';
+import {ThemeProvider} from "./context/ThemeContext";
 
 const Stack = createStackNavigator();
 
-const App = () => {
+const app = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <ThemeProvider>
+            <NavigationContainer>
+                    <Stack.Navigator initialRouteName="Home">
+                        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+                    </Stack.Navigator>
+            </NavigationContainer>
+        </ThemeProvider>
     );
 }
 
-export default App;
+export default app;
