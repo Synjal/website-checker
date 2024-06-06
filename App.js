9d400e40deeb6e853de70df945e5aedfb577c47e
Synjal <frau.nicolas7@gmail.com>
2024-06-06 16:18:08 +0200

    add - Ping context and a second screen "details"

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useContext, useEffect} from 'react';
+import {NavigationContainer, useRoute} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './app/Home';
 import {ThemeProvider} from "./context/ThemeContext";
+import DetailsScreen from "./screens/DetailsScreen";
+import {PingProvider} from "./context/PingContext";
 
 const Stack = createStackNavigator();
 
 const app = () => {
+
     return (
         <ThemeProvider>
-            <NavigationContainer>
-                    <Stack.Navigator initialRouteName="Home">
-                        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-                    </Stack.Navigator>
-            </NavigationContainer>
+            <PingProvider>
+                <NavigationContainer>
+                        <Stack.Navigator initialRouteName="Home">
+                            <Stack.Screen
+                                name="Home"
+                                component={Home}
+                                options={{ headerShown: false }}
+                            />
+                            <Stack.Screen
+                                name="Details"
+                                component={DetailsScreen}
+                                options={{ headerShown: false }}
+                            />
+                        </Stack.Navigator>
+                </NavigationContainer>
+            </PingProvider>
         </ThemeProvider>
     );
 }
