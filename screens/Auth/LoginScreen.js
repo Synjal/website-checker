e9a7cbb28ec83a1ea471ae168e13ca4b7a6e7c18
Synjal <frau.nicolas7@gmail.com>
2024-06-11 13:29:41 +0200

    add - authentification screens

---
+++ screens/Auth/LoginScreen.js
@@ -0,0 +1,182 @@
+import React, { useContext, useState } from 'react';
+import { KeyboardAvoidingView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { validate } from 'email-validator';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {Button, Icon, TextInput} from "react-native-paper";
+import {ThemeContext} from "../../context/ThemeContext";
+import {auth} from "../../constants/Server";
+import Loader from "../../components/Loader";
+
+const LoginScreen = ({ navigation }) => {
+    const { theme } = useContext(ThemeContext);
+
+    const [userEmail, setUserEmail] = useState('test@hotmail.fr');
+    const [userPassword, setUserPassword] = useState('Password');
+    const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [errorText, setErrorText] = useState('');
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleValidation = () => {
+        if (!userEmail) {
+            setErrorText('Il manque votre email');
+            return false;
+        }
+        if (!validate(userEmail)) {
+            setErrorText('Email invalide');
+            return false;
+        }
+        if (!userPassword) {
+            setErrorText('Et votre mot de passe, alors ?');
+            return false;
+        }
+        return true;
+    };
+
+    const handleSubmitPress = async () => {
+        setErrorText('');
+        if (!handleValidation()) {
+            return;
+        }
+        setLoading(true);
+
+        try {
+            const response = await axios.post(auth, {
+                email: userEmail,
+                password: userPassword
+            });
+            await AsyncStorage.setItem('jwtToken', JSON.stringify(response.data));
+            setLoading(false)
+            navigation.navigate('Home');
+        } catch (error) {
+            setLoading(false)
+            alert('Une erreur est survenue. Veuillez réessayer.');
+        }
+    };
+
+    return (
+        <View style={styles.mainBody(theme)}>
+            <Loader loading={loading} />
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ flex: 1, justifyContent: 'center', alignContent: 'center', }}>
+                <View>
+                    <KeyboardAvoidingView enabled>
+                        <View style={styles.logoContainer}>
+                            <Icon
+                                source="web"
+                                color={theme.primary}
+                                size={120}
+                            />
+                        </View>
+                        <TextInput
+                            style={styles.inputStyle}
+                            label="Email"
+                            onChangeText={setUserEmail}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            textColor={theme.onBackground}
+                        />
+                        <TextInput
+                            style={styles.inputStyle}
+                            label="Mot de passe"
+                            onChangeText={setUserPassword}
+                            secureTextEntry={!showPassword}
+                            textColor={theme.onBackground}
+                            right={
+                                <TextInput.Icon
+                                    icon={showPassword ? 'eye' : 'eye-off'}
+                                    onPress={toggleShowPassword}
+                                    style={styles.eyeIcon(theme)}
+                                />
+                            }
+                        />
+                        {errorText !== '' && (
+                            <Text style={styles.errorTextStyle(theme)}>{errorText}</Text>
+                        )}
+                        <Button
+                            style={styles.buttonStyle(theme)}
+                            mode="outlined"
+                            labelStyle={styles.text(theme)}
+                            onPress={handleSubmitPress}
+                        >
+                            Se connecter
+                        </Button>
+                        <Text
+                            style={styles.registerTextStyle(theme)}
+                            onPress={() => navigation.navigate('RegisterScreen')}>
+                            Pas encore de compte ? C'est par ici !
+                        </Text>
+                    </KeyboardAvoidingView>
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+    mainBody: theme => ({
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: theme.background
+    }),
+    logoContainer:{
+        alignItems: 'center',
+        margin: 20,
+        marginBottom: 100,
+    },
+    logoImage: {
+        width: '50%',
+        height: 100,
+        resizeMode: 'contain',
+        margin: 30,
+    },
+    buttonStyle: theme => ({
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginHorizontal: 50,
+        marginVertical: 80,
+        marginBottom: 80,
+        backgroundColor: theme.primary,
+    }),
+    text: theme => ({
+        fontSize: 13,
+        letterSpacing: 0.33,
+        fontWeight: '500',
+        textTransform: 'uppercase',
+        color: theme.onPrimary,
+    }),
+    inputStyle:{
+        marginHorizontal: 35,
+        marginVertical: 10,
+        backgroundColor: 'transparent',
+        borderRadius: 30,
+    },
+    registerTextStyle: theme => ({
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 14,
+        alignSelf: 'center',
+        margin: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: theme.tertiary,
+        color: theme.tertiary,
+    }),
+    errorTextStyle: theme => ({
+        textAlign: 'center',
+        fontSize: 14,
+        color: theme.error,
+    }),
+    eyeIcon: (theme) => ({
+        backgroundColor: theme.background,
+    }),
+});
---
+++ screens/Auth/RegisterScreen.js
@@ -0,0 +1,221 @@
+import {View, Text, Image, TouchableOpacity, SafeAreaView, KeyboardAvoidingView, ScrollView, StyleSheet} from "react-native";
+import React, {createRef, useContext, useState} from "react";
+import {ThemeContext} from "../../context/ThemeContext";
+import {validate} from "email-validator";
+import axios from "axios";
+import {register} from "../../constants/Server";
+import {HelperText, Icon, TextInput} from "react-native-paper";
+import Loader from "../../components/Loader";
+
+const RegisterScreen = ({ navigation }) => {
+    const { theme } = useContext(ThemeContext);
+
+    const [userEmail, setUserEmail] = useState('');
+    const emailInputRef = createRef();
+    const [emailError, setEmailError] = useState(false);
+
+    const [userPassword, setUserPassword] = useState('');
+    const passwordInputRef = createRef();
+    const [passwordError, setPasswordError] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmitButton = async () => {
+        if (!emailError && !passwordError && validate(userEmail)) {
+            setLoading(true);
+            try {
+                const response = await axios.post(register, {
+                    email: userEmail,
+                    roles: ['USER'],
+                    password: userPassword,
+                }, {
+                    headers: {
+                        'Content-Type': 'application/ld+json',
+                        'Accept': 'application/ld+json'
+                    },
+                    withCredentials: false
+                });
+
+                if (response.status === 201) {
+                    console.log("Account created")
+                    return (
+                        <SafeAreaView style={styles.SuccessWindow(theme)}>
+
+                            <Text style={styles.successTextStyle(theme)}>
+                                Inscription réussie
+                            </Text>
+                            <TouchableOpacity
+                                style={styles.buttonStyle(theme)}
+                                activeOpacity={0.5}
+                                onPress={() => props.navigation.navigate('LoginScreen')}>
+                                <Text style={styles.buttonTextStyle(theme)}>Se connecter</Text>
+                            </TouchableOpacity>
+                        </SafeAreaView>
+                    );
+                }
+            } catch (error) {
+                console.log(error)
+                alert('Une erreur est survenue. Merci de réessayer plus tard.')
+            } finally {
+                setLoading(false)
+            }
+        } else console.log("Erreur de validation des champs")
+    }
+
+    return (
+        <SafeAreaView style={styles.MainBody(theme)}>
+            <Loader loading={loading} />
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{justifyContent: 'center', alignContent: 'center',}}>
+                <View style={styles.logoContainer}>
+                    <Icon
+                        source="web"
+                        color={theme.primary}
+                        size={120}
+                    />
+                </View>
+                <KeyboardAvoidingView enabled>
+                    <View style={styles.SectionStyle}>
+                        <View style={styles.inputContainer}>
+                            <TextInput
+                                style={styles.inputStyle(theme)}
+                                onChangeText={(UserEmail) => {
+                                    setUserEmail(UserEmail)
+                                    setEmailError(UserEmail === '' || !validate(UserEmail))
+                                }}
+                                label="Email"
+                                keyboardType="email-address"
+                                ref={emailInputRef}
+                                returnKeyType="next"
+                                onSubmitEditing={() => passwordInputRef.current && passwordInputRef.current.focus()}
+                                blurOnSubmit={false}
+                                error={emailError}
+                                textColor={theme.onBackground}
+                            />
+                            <HelperText type="error" visible={emailError} style={styles.errorTextStyle(theme)}>
+                                {userEmail === '' ? "Vous avez oublié votre mail !" : "Votre mail n'est pas au bon format."}
+                            </HelperText>
+                        </View>
+                    </View>
+                    <View style={styles.SectionStyle}>
+                        <View style={styles.inputContainer}>
+                            <TextInput
+                                style={styles.inputStyle(theme)}
+                                onChangeText={(UserPassword) => {
+                                    setUserPassword(UserPassword)
+                                    setPasswordError(UserPassword === '')
+                                }}
+                                label="Mot de passe"
+                                ref={passwordInputRef}
+                                returnKeyType="next"
+                                secureTextEntry={!showPassword}
+                                blurOnSubmit={false}
+                                error={passwordError}
+                                textColor={theme.onBackground}
+                                right={
+                                    <TextInput.Icon
+                                        icon={showPassword ? 'eye' : 'eye-off'}
+                                        onPress={toggleShowPassword}
+                                        style={styles.eyeIcon(theme)}
+                                    />
+                                }
+                            />
+                            <HelperText type="error" visible={passwordError} style={styles.errorTextStyle(theme)}>
+                                {userPassword === ''
+                                    ? "Vous avez oublié le mot de passe !"
+                                    : "Votre mot de passe doit contenir au moins 1 chiffre, une lettre minuscule, " +
+                                    "une lettre majuscule, un caractère spécial et faire au moins 8 caractères. !"}
+                            </HelperText>
+                        </View>
+                    </View>
+                    <TouchableOpacity style={styles.buttonStyle(theme)} onPress={handleSubmitButton}>
+                        <Text style={styles.buttonTextStyle(theme)}>Je m'inscris !</Text>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+    MainBody: theme => ({
+        flex: 1,
+        backgroundColor: theme.background,
+    }),
+    logoContainer: {
+        alignItems: 'center',
+        margin: 20,
+        marginTop: 60,
+        marginBottom: 100,
+    },
+    logoImage: {
+        width: '50%',
+        height: 100,
+        resizeMode: 'contain',
+        margin: 30,
+    },
+    SectionStyle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 35,
+        marginRight: 35,
+    },
+    inputContainer: {
+        flex: 1,
+    },
+    inputStyle: theme => ({
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15,
+        backgroundColor: theme.background,
+    }),
+    buttonStyle: theme => ({
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginHorizontal: 50,
+        marginVertical: 20,
+        backgroundColor: theme.primary,
+    }),
+    buttonTextStyle: theme => ({
+        fontSize: 13,
+        letterSpacing: 0.33,
+        fontWeight: '500',
+        textTransform: 'uppercase',
+        color: theme.onPrimary,
+    }),
+    errorTextStyle: theme => ({
+        textAlign: "left",
+        fontSize: 12,
+        marginBottom: 10,
+        color: theme.error,
+    }),
+    successTextStyle: theme => ({
+        textAlign: 'center',
+        fontSize: 18,
+        padding: 30,
+        color: theme.onBackground,
+    }),
+    SuccessWindow: theme => ({
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: theme.background,
+    }),
+    SuccessImage: {
+        height: 150,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+    },
+    eyeIcon: (theme) => ({
+        backgroundColor: theme.background,
+    }),
+});
