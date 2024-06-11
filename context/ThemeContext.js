4ad7642dc35474702a54c123c709ab7b4fd2b9b8
Synjal <frau.nicolas7@gmail.com>
2024-06-11 10:46:43 +0200

    fix

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,34 +1,33 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {LightTheme} from "../constants/LightTheme";
+import { LightTheme } from "../constants/LightTheme";
 
-export const ThemeContext = createContext('')
+export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState(LightTheme)
+    const [theme, setTheme] = useState(LightTheme);
 
     useEffect(() => {
         const initTheme = async () => {
             const storedTheme = await AsyncStorage.getItem("theme");
             if (storedTheme) {
                 setTheme(JSON.parse(storedTheme));
+            } else {
+                setTheme(LightTheme);
             }
-        }
+        };
 
-        initTheme()
-    }, [])
+        initTheme();
+    }, []);
 
     const updateTheme = async (newTheme) => {
         setTheme(newTheme);
-        await AsyncStorage.setItem("theme", JSON.stringify(newTheme))
+        await AsyncStorage.setItem("theme", JSON.stringify(newTheme));
     };
 
     return (
-        <ThemeContext.Provider value={{
-            theme,
-            updateTheme,
-        }}>
+        <ThemeContext.Provider value={{ theme, updateTheme }}>
             {children}
         </ThemeContext.Provider>
-    )
-}
+    );
+};
