ac7f87794a1bc2ac9b831caf8d991bc574de8366
Synjal <frau.nicolas7@gmail.com>
2024-06-11 15:51:40 +0200

    add - ressources paths

--- constants/Server.js
+++ constants/Server.js
@@ -1,4 +1,11 @@
+// SERVER MAIN PATH
 export const server = 'https://website-checker.boreales-creations.fr';
 
+// AUTH PATHS
 export const auth = server + '/auth';
 export const register = server + '/api/users';
+
+// RESSOURCES PATHS
+export const websites = server + '/api/websites';
+export const users = server + '/api/users';
+
