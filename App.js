90c0f7a524e3f773be173e311ba8a2b179f34438
Synjal <frau.nicolas7@gmail.com>
2024-06-11 10:47:43 +0200

    add - auth route

--- App.js
+++ App.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './app/Home';
-import {ThemeProvider} from "./context/ThemeContext";
+import {ThemeContext, ThemeProvider} from "./context/ThemeContext";
 import DetailsScreen from "./screens/DetailsScreen";
 import {PingProvider} from "./context/PingContext";
+import LoginScreen from "./screens/Auth/LoginScreen";
+import RegisterScreen from "./screens/Auth/RegisterScreen";
 
 const Stack = createStackNavigator();
 
-const app = () => {
+const Auth = () => {
+    const { theme } = useContext(ThemeContext);
 
+    return (
+        <Stack.Navigator initialRouteName="LoginScreen">
+            <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{
+                title: 'Inscription',
+                headerStyle: {backgroundColor: theme.primary,},
+                headerTintColor: theme.onPrimary,
+                headerTitleStyle: {fontWeight: 'bold',},
+            }}
+            />
+        </Stack.Navigator>
+    );
+};
+
+const App = () => {
     return (
         <ThemeProvider>
             <PingProvider>
                 <NavigationContainer>
-                        <Stack.Navigator initialRouteName="Home">
-                            <Stack.Screen
-                                name="Home"
-                                component={Home}
-                                options={{ headerShown: false }}
-                            />
-                            <Stack.Screen
-                                name="Details"
-                                component={DetailsScreen}
-                                options={{ headerShown: false }}
-                            />
-                        </Stack.Navigator>
+                    <Stack.Navigator initialRouteName="Auth">
+                        <Stack.Screen name="Auth" component={Auth} options={{headerShown: false}}/>
+                        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+                        <Stack.Screen name="Details" component={DetailsScreen} options={{ headerShown: false }}/>
+                    </Stack.Navigator>
                 </NavigationContainer>
             </PingProvider>
         </ThemeProvider>
     );
 }
 
-export default app;
+export default App;
